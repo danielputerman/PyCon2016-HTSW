f03ca9f45203b1d147c9ac96a573b616e5900636
danielputerman <danielputerman@users.noreply.github.com>
2016-04-30 13:12:01 +0300

    Inital script

---
+++ basic_script.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+try:
+    browser.get('http://il.pycon.org/2016/')
+
+    signup_link = browser.find_element_by_link_text('sign up to our list')
+    link_location = signup_link.location
+    scroll_script = "scrollTo({},{})".format(link_location['x'], link_location['y'] - 70)
+    browser.execute_script(scroll_script)
+
+    signup_link.click()
+
+    browser.find_element_by_class_name('btn-waitlist').click()
+
+    browser.find_element_by_id('waitlisted_person_name').send_keys('Selenium WebDriver')
+
+finally:
+    browser.quit()
